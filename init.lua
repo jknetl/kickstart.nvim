e88e3a52108b3815411735473380cfe403a2db1e
Jakub Knetl <knetl.j@gmail.com>
2025-02-20 06:34:56 +0100

    Add oil nvim

--- init.lua
+++ init.lua
@@ -1068,6 +1068,17 @@ require('lazy').setup({
       return require('tmux').setup()
     end,
   },
+  {
+    'stevearc/oil.nvim',
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    -- Optional dependencies
+    dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+    lazy = false,
+  },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
